0d42186997ad151cc14e89dac2b116164bb66d75
Mike Green <mike.is.green@gmail.com>
2010-11-19 20:39:16 -0500

    trying to make an AJAX request to the API in background.html (via background.js), but so far no joy

--- js/background.js
+++ js/background.js
@@ -1,9 +1,38 @@
 $(document).ready(function() {
 	// TODO: Create badge w/ total hours worked today via chrome.browserAction.setBadgeText({text: "0:00"})
 	
-	var authString = localStorage['harvest_auth_string'];
+	var authString = localStorage['harvest_auth_string']
+		, subdomain = localStorage['harvest_subdomain']
+		, hurl = 'https://' + subdomain + '.harvestapp.com';
 
-	if (!authString) {
+	if (_.isEmpty(authString) || _.isEmpty(subdomain)) {
 		chrome.browserAction.setBadgeText({text: "!"});
+	} else {
+		$.ajax({
+			url: hurl + '/daily'
+			, type: 'GET'
+			, dataType: 'xml'
+			, beforeSend: function(xhr) {
+				xhr.setRequestHeader('Authorization', 'Basic ' + authString);
+				xhr.setRequestHeader('Accept', 'application/xml');
+				xhr.setRequestHeader('Content-Type', 'application/xml');
+				return true;
+			}
+			, success: function(xml) {
+				console.log('success');
+				var hours = 0.0 
+					, $dayEntries = $(xml).find('day_entries').find('day_entry');
+				
+				$dayEntries.each(function() {
+					var hrs = $(this).find('hours').text()
+						, floatHrs = parseFloat(hrs);
+					hours += floatHrs;
+				});
+				chrome.browserAction.setBadgeText({text: '' + hours });
+				console.log(hours);
+			}
+		});
 	}
 });
+
+// vim: set ts=2 sw=2 syntax=jquery smartindent :
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
 	var bgPage = chrome.extension.getBackgroundPage();
+
 });
