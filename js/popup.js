452628264499a0af0dd80744988b6ca0af46674f
Mike Green <mike.is.green@gmail.com>
2010-11-23 10:28:12 -0500

    added a default row to the popup.html entry table that says 'no entries today'

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	var bgPage = chrome.extension.getBackgroundPage();
 	$.each(bgPage.application.todaysEntries, function() {
+		if (this.hours) {
+			$('.noentries').remove();
+		}
 		$('#timesheet tbody').append('<tr><td>'+this.client+'</td><td>'+this.hours+'</td>');
 	});
 });
