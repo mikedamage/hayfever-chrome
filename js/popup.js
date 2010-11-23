05317406ad084b0154831847f32e7507cf12b12f
Mike Green <mike.is.green@gmail.com>
2010-11-23 12:48:32 -0500

    abandoning Mustach in favor of a different solution

--- js/popup.js
+++ js/popup.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
 	$.each(bgPage.application.todaysEntries, function() {
 		if (this.hours) {
 			$('.noentries').remove();
-			if (Views) {
-				var row = Mustache.to_html(Views['entry'], this);
-				$('#timesheet tbody').append(row);
-			}
 		}
 	});
 });
