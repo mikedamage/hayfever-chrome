436249c52f5796660ea9216336f59fb4aac675cc
Mike Green <mike.is.green@gmail.com>
2011-02-27 00:53:44 -0500

    Google Analytics are off by default, and configurable via a checkbox on the options page

--- js/options.js
+++ js/options.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var $subdomainField = $('#harvest-subdomain')
 		, $usernameField = $('#harvest-username')
 		, $passwordField = $('#harvest-password')
+		, $formFields = $('input[type!="submit"]')
 		, subdomain = localStorage['harvest_subdomain']
 		, username = localStorage['harvest_username']
 		, authString = localStorage['harvest_auth_string']
@@ -16,7 +17,6 @@ $(document).ready(function() {
 		$usernameField.val(username);
 	}
 
-	// TODO: Don't store password in production version. Just store base64 encoded auth string and tell user that their password is saved.
 	if (authString) {
 		$('#harvest-password').next('.hint').html("We've successfully used your password to build an authentication string. This field will remain blank because we don't actually store your password. You can enter a new password at any time to rebuild the authentication string.");
 	}
@@ -30,20 +30,46 @@ $(document).ready(function() {
 	}
 
 	$('#options').submit(function() {
-		if ($subdomainField.val().length > 0) {
-			localStorage['harvest_subdomain'] = $subdomainField.val();
-		}
-		if ($usernameField.val().length > 0) {
-			localStorage['harvest_username'] = $usernameField.val();
-		}
-		if ($passwordField.val().length > 0) {
-			var tempPassword = $passwordField.val();
-		}
+		
+		var prefs = {}, tempPassword;
+
+		$formFields.each(function() {
+			var fieldName = $(this).attr('name')
+				, fieldVal = $(this).val();
+
+			switch (fieldName) {
+				case 'harvest_subdomain':
+					/*jsl:fallthru*/
+				case 'harvest_username':
+					if (fieldVal.length > 0) {
+						localStorage.setItem(fieldName, fieldVal);
+					}
+				break;
+				case 'harvest_password':
+					tempPassword = fieldVal;
+				break;
+				case 'enable_analytics':
+					if ($(this).is(':checked')) {
+						prefs.enable_analytics = true;
+					} else {
+						prefs.enable_analytics = false;
+					}
+				break;
+				default:
+					prefs[fieldName] = fieldVal;
+			}
+		});
 
 		if (localStorage['harvest_username'] && tempPassword) {
 			var b64AuthString = btoa(localStorage['harvest_username'] + ':' + tempPassword);
-			localStorage['harvest_auth_string'] = b64AuthString;
+			localStorage.setItem('harvest_auth_string', b64AuthString);
 		}
+
+		if (!prefs.hasOwnProperty('enable_analytics')) {
+			prefs.enable_analytics = false;
+		}
+
+		localStorage.setItem('hayfever_prefs', JSON.stringify(prefs));
 
 		$('#status').addClass('success').html('Options successfully saved').fadeIn(400);
 
