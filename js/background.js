21ae10925c50fb10d7b23d8af0a02e02ad0350b1
Mike Green <mike.is.green@gmail.com>
2011-04-15 11:04:42 -0400

    Version bump to 0.2.1

--- js/background.js
+++ js/background.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 		, subdomain = localStorage['harvest_subdomain'];
 
 	window.application = {
-		version: '0.2.0'
+		version: '0.2.1'
 		, totalHours: 0.0
 		, currentHours: 0.0
 		, todaysEntries: []
