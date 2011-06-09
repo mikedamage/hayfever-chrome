3ededb1eb5e0f55eb39959691269a308d3881011
Mike Green <mike.is.green@gmail.com>
2011-06-09 00:10:43 -0400

    Version bump to 0.2.4 in manifest.json and background.js. Need to test more before releasing

--- js/background.js
+++ js/background.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 		, subdomain = localStorage['harvest_subdomain'];
 
 	window.application = {
-		version: '0.2.3'
+		version: '0.2.4'
 		, authorized: false
 		, totalHours: 0.0
 		, currentHours: 0.0
