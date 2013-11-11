2861940b3f1523ec0d8dc996fd83172dd2cc038a
Mike Green <mike.is.green@gmail.com>
2013-11-11 16:59:20 -0500

    Moved all plugin production stuff into dist folder

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path       = "/"
-css_dir         = "css"
-sass_dir        = "sass"
-images_dir      = "img"
-javascripts_dir = "js"
+css_dir         = "dist/css"
+sass_dir        = "dist/sass"
+images_dir      = "dist/img"
+javascripts_dir = "dist/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
