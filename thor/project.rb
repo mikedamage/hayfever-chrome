158879bd5ca8f27b0155689d6cd13b773e624041
Mike Green <mike.is.green@gmail.com>
2013-12-28 15:08:24 -0500

    Removed project:prep_release thor task b/c it's not needed anymore

--- thor/project.rb
+++ thor/project.rb
@@ -2,29 +2,14 @@
 class Project < Thor
 	include Thor::Actions
 
-	@@excludes = [
-		'.git',
-		'.gitignore',
-		'Thorfile',
-		'.DS_Store',
-		'sass',
-		'.sass-cache',
-		'test',
-		'coffeescripts',
-		'config.rb',
-		'.rvmrc',
-		'Gemfile',
-		'Gemfile.lock'
-	].map {|exc| "--exclude=#{exc}"}.join(' ')
-
-  desc 'build', 'Compile all assets (CoffeeScript and Sass)'
+  desc 'build', 'Compile all Sass and CoffeeScript assets'
   def build
     thor "css:compile"
     thor "coffeescript:compile"
   end
 
-	desc 'bundle [OUTPUT_DIR]', 'Bundle the project into a zip file'
-	def bundle(output_dir = $root_dir.join('pkg'))
+	desc 'zip_release [OUTPUT_DIR]', 'Bundle the project into a zip file'
+	def zip_release(output_dir = $root_dir.join('pkg'))
     require 'json'
     require 'zip'
 
@@ -56,26 +41,5 @@ class Project < Thor
     end
 
     say_status 'saved', "Saved plugin to #{output.to_s}", :green
-	end
-
-	desc 'prep_release', 'Syncs all necessary files to ~/ProjectFiles/Hayfever/hayfever in preparation for packing in Chrome'
-	def prep_release
-		invoke 'css:compile'
-		invoke 'coffeescript:compile'
-
-		sync_dir = Pathname.new("~/ProjectFiles/Hayfever/hayfever")
-		
-		unless sync_dir.directory?
-			say_status 'mkdir', 'Creating sync directory', :yellow
-			`mkdir -p ~/ProjectFiles/Hayfever/hayfever`
-		end
-
-		say 'Copying files to ~/ProjectFiles/Hayfever/hayfever', :blue
-		IO.popen("rsync -avr #{@@excludes} --delete ./ ~/ProjectFiles/Hayfever/hayfever/") do |rsync|
-			while line = rsync.gets
-				say line
-			end
-		end
-		say 'Done.', :green
 	end
 end
